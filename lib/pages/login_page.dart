864040b6864fef26e472c8c69d5fea5761b3ed95
Shaishav Surati <shaishavsurati06@gmail.com>
2021-02-22 18:07:33 +0530

    Day5 Complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,57 +1,60 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(children: <Widget>[
-        Image.asset("assets/images/login_image.png",
-        fit: BoxFit.cover),
-        SizedBox(
-          height: 20.0,
-          ),
-        Text("Welcome",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          ),
-          ),
+      child: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Image.asset("assets/images/login_image.png",
+          fit: BoxFit.cover),
           SizedBox(
-          height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal:32.0),
-            child: Column(children: <Widget>[
-              TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter UserName",
-                labelText: "UserName",
-              ),
+            height: 20.0,
             ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                labelText: "Password",
-              ),
+          Text("Welcome",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            ),
             ),
             SizedBox(
-              height: 20.0,
-          ),
-          FlatButton(
-            child: Text("Login"),
-            autofocus: true,
-            textColor: Colors.white,
-            color: Colors.deepPurple,
-            hoverColor: Colors.blue,
-            onPressed: (){
-              print("Hello!!! Shaishav");
-            },),      
-            ],
+            height: 20.0,
             ),
-          )
-      ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal:32.0),
+              child: Column(children: <Widget>[
+                TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter UserName",
+                  labelText: "UserName",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+            ),
+            FlatButton(
+              child: Text("Login"),
+              autofocus: true,              
+              textColor: Colors.white,
+              color: Colors.deepPurple,
+              hoverColor: Colors.blue,
+              onPressed: (){
+                Navigator.pushNamed(context,MyRoutes.homeRoute);
+              },),
+              ],
+              ),
+            )
+        ],
+        ),
       ),
     );
   }
