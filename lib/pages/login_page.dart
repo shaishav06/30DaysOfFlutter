3f44a4029c2cd5e28cda968b6a513bafa1fc1e7e
Shaishav Surati <shaishavsurati06@gmail.com>
2021-02-22 17:26:05 +0530

    Day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,14 +4,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 40,
-          color: Colors.blue,
+      color: Colors.white,
+      child: Column(children: <Widget>[
+        Image.asset("assets/images/login_image.png",
+        fit: BoxFit.cover),
+        SizedBox(
+          height: 20.0,
+          ),
+        Text("Welcome",
+        style: TextStyle(
+          fontSize: 24,
           fontWeight: FontWeight.bold,
-        ),
-      )
-    ),
+          ),
+          ),
+          SizedBox(
+          height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal:32.0),
+            child: Column(children: <Widget>[
+              TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter UserName",
+                labelText: "UserName",
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+          ),
+          FlatButton(
+            child: Text("Login"),
+            autofocus: true,
+            textColor: Colors.white,
+            color: Colors.deepPurple,
+            hoverColor: Colors.blue,
+            onPressed: (){
+              print("Hello!!! Shaishav");
+            },),      
+            ],
+            ),
+          )
+      ],
+      ),
     );
   }
 }
